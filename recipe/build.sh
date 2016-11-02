36ae63a3d0eadde1318b10642354a905b7fef032
Andreas Kloeckner <inform@tiker.net>
2016-11-02 18:29:55 -0500

    Remove OProfile support (seems broken in LLVM 3.9)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,26 +1,9 @@
-platform='unknown'
-unamestr="$(uname)"
-machine="$(uname -m)"
-
-if [[ "$unamestr" == 'Linux' ]]; then
-    platform='linux'
-elif [[ "$unamestr" == 'FreeBSD' ]]; then
-    platform='freebsd'
-elif [[ "$unamestr" == 'Darwin' ]]; then
-    platform='osx'
-fi
-
 # Note you may need to enable RH devtoolset-2 if building on an
 # old RH or CentOS system
 
 # Use CMake-based build procedure
 mkdir build
 cd build
-if [ "$platform" == 'linux' -a "$machine" != 'armv7l' ]; then
-    USE_OPROFILE="ON"
-else
-    USE_OPROFILE="OFF"
-fi
 
 cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
       -DCMAKE_BUILD_TYPE=Release \
@@ -31,7 +14,6 @@ cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
       -DLLVM_INCLUDE_DOCS=OFF \
       -DLLVM_INCLUDE_EXAMPLES=OFF \
       -DLLVM_ENABLE_TERMINFO=OFF \
-      -DLLVM_USE_OPROFILE=$USE_OPROFILE \
       ..
 
 make -j 8
