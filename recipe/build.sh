ff90230c7dc28edeb7dbac00cb42d0f5cb3ab179
Jonathan Ragan-Kelley <jrk@google.com>
2017-03-07 13:22:14 -0800

    Enable ccache on Jenkins + CircleCI
    This should dramatically accelerate builds, and 
    allow them to make incremental progress across 
    runs that time out.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,13 @@
 mkdir build
 cd build
 
+if [ -x "$(command -v ccache)" ]; then
+  ENABLE_CCACHE=ON
+else
+  ENABLE_CCACHE=OFF
+  echo "WARNING: Failed to find ccache"
+fi
+
 cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
       -DCMAKE_BUILD_TYPE=Release \
       -DLLVM_ENABLE_RTTI=ON \
@@ -9,6 +16,7 @@ cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
       -DLLVM_INCLUDE_DOCS=OFF \
       -DLLVM_INCLUDE_EXAMPLES=OFF \
       -DLLVM_ENABLE_TERMINFO=OFF \
+      -DLLVM_CCACHE_BUILD=$ENABLE_CCACHE \
       ..
 
 echo "Building -j${CPU_COUNT}"
