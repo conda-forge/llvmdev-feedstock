b25514a66c139a2fb07e3760273770d14b1ab6de
Isuru Fernando <isuruf@gmail.com>
2019-08-09 01:36:50 +0000

    Fix packaging

--- recipe/install_llvm.sh
+++ recipe/install_llvm.sh
@@ -1,5 +1,20 @@
 cd build
 make install
-if [[ "${PKG_NAME}" == "llvmdev" ]]; then
-    rm $PREFIX/lib/libLLVM-*${SHLIB_EXT}*
+
+IFS='.' read -ra VER_ARR <<< "$PKG_VERSION"
+
+if [[ "${PKG_NAME}" == libllvm* ]]; then
+    rm -rf $PREFIX/bin
+    rm -rf $PREFIX/include
+    rm -rf $PREFIX/share
+    mv $PREFIX/lib $PREFIX/lib2
+    mkdir -p $PREFIX/lib
+    mv $PREFIX/lib2/libLLVM-${VER_ARR[0]}${SHLIB_EXT} $PREFIX/lib
+    mv $PREFIX/lib2/lib*.so.${VER_ARR[0]} $PREFIX/lib || true
+    mv $PREFIX/lib2/lib*.${VER_ARR[0]}.dylib $PREFIX/lib || true
+    rm -rf $PREFIX/lib2
+elif [[ "${PKG_NAME}" == "llvm-tools" ]]; then
+    rm -rf $PREFIX/lib
+    rm -rf $PREFIX/include
 fi
+
