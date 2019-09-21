728e068c9c93b0fae509950139e94c9682a28527
Isuru Fernando <isuruf@gmail.com>
2019-09-21 21:09:07 +0000

    Use relative paths

--- recipe/install_llvm.bat
+++ recipe/install_llvm.bat
@@ -8,9 +8,3 @@ if "%PKG_NAME%" == "llvm-tools" (
     rmdir /S /Q %LIBRARY_INC%
     rmdir /S /Q %LIBRARY_PREFIX%\libexec
 )
-
-if "%PKG_NAME%" == "llvmdev" (
-    set "FILE=%LIBRARY_LIB%\\cmake\\llvm\\LLVMExports-release.cmake"
-    sed -i 's@'%LIBRARY_PREFIX%'@${_IMPORT_PREFIX}@g' %FILE%
-    type %FILE%
-)
