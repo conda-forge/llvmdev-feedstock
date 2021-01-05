fb46ac99c34ae7154e8e4b63d80fd396c564e0ce
Uwe L. Korn <uwe.korn@quantco.com>
2021-01-05 16:34:31 +0100

    Apply patches from other LLVM PRs

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,13 +11,9 @@ cd build
       -DLLVM_USE_INTEL_JITEVENTS=ON
 "
 
-if [[ "$target_platform" == "osx-64" ]]; then
-    export CONDA_BUILD_SYSROOT_BACKUP=${CONDA_BUILD_SYSROOT}
-    conda install -p $BUILD_PREFIX --quiet --yes clangxx_osx-64=${cxx_compiler_version}
-    export CONDA_BUILD_SYSROOT=${CONDA_BUILD_SYSROOT_BACKUP}
-    export CFLAGS="$CFLAGS -isysroot $CONDA_BUILD_SYSROOT"
-    export CXXFLAGS="$CXXFLAGS -isysroot $CONDA_BUILD_SYSROOT"
-    export LDFLAGS="$LDFLAGS -isysroot $CONDA_BUILD_SYSROOT"
+if [[ "$CC_FOR_BUILD" != "" && "$CC_FOR_BUILD" != "$CC" ]]; then
+  CMAKE_ARGS="${CMAKE_ARGS} -DCROSS_TOOLCHAIN_FLAGS_NATIVE=-DCMAKE_C_COMPILER=$CC_FOR_BUILD;-DCMAKE_CXX_COMPILER=$CXX_FOR_BUILD;-DCMAKE_EXE_LINKER_FLAGS=;-DCMAKE_MODULE_LINKER_FLAGS=;-DCMAKE_SHARED_LINKER_FLAGS=;-DCMAKE_C_FLAGS=-O2;-DCMAKE_CXX_FLAGS=-O2"
+  CMAKE_ARGS="${CMAKE_ARGS} -DLLVM_HOST_TRIPLE=$(echo $HOST | sed s/conda/unknown/g) -DLLVM_DEFAULT_TARGET_TRIPLE=$(echo $HOST | sed s/conda/unknown/g)"
 fi
 
 cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
@@ -37,7 +33,9 @@ cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
       -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly \
       -DLLVM_BUILD_LLVM_DYLIB=yes \
       -DLLVM_LINK_LLVM_DYLIB=yes \
-      ${conditional_args} ..
+      ${conditional_args} \
+      ${CMAKE_ARGS} \
+      ..
 
 make -j${CPU_COUNT}
 
