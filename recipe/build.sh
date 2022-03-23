71554e9018ecc9646ca83e249130ccffedb35dfa
H. Vetinari <h.vetinari@gmx.com>
2022-03-23 19:04:16 +1100

    use llvm-project sources directly; also drop obsolete patch
    no need to use two sources to work-around upstream issue 53281

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,8 @@
 set -x
 
 # Make osx work like linux.
-sed -i.bak "s/NOT APPLE AND ARG_SONAME/ARG_SONAME/g" cmake/modules/AddLLVM.cmake
-sed -i.bak "s/NOT APPLE AND NOT ARG_SONAME/NOT ARG_SONAME/g" cmake/modules/AddLLVM.cmake
-
-# Workaround https://github.com/llvm/llvm-project/issues/53281
-cp llvm-project/cmake/Modules/* cmake/modules/
+sed -i.bak "s/NOT APPLE AND ARG_SONAME/ARG_SONAME/g" llvm/cmake/modules/AddLLVM.cmake
+sed -i.bak "s/NOT APPLE AND NOT ARG_SONAME/NOT ARG_SONAME/g" llvm/cmake/modules/AddLLVM.cmake
 
 mkdir build
 cd build
@@ -45,7 +42,8 @@ cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
       -DLLVM_LINK_LLVM_DYLIB=yes \
       -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly \
       ${CMAKE_ARGS} \
-      -GNinja ..
+      -GNinja \
+      ../llvm
 
 ninja
 
@@ -64,6 +62,6 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
 
   ninja check-llvm
 
-  cd ../test
-  ../build/bin/llvm-lit -vv Transforms ExecutionEngine Analysis CodeGen/X86
+  cd ../llvm/test
+  ../../build/bin/llvm-lit -vv Transforms ExecutionEngine Analysis CodeGen/X86
 fi
