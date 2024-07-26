6ef8e8292c1526624378e6bd426a914453995506
H. Vetinari <h.vetinari@gmx.com>
2024-07-27 09:44:36 +1100

    adapt to new scheme for library suffixes

--- recipe/install_llvm.sh
+++ recipe/install_llvm.sh
@@ -11,9 +11,10 @@ MAJOR_EXT="${VER_ARR[0]}"
 SOVER_EXT="${VER_ARR[0]}.${VER_ARR[1]}"
 # for rc's, both MAJOR_EXT & SOVER_EXT get suffixes
 if [[ "${PKG_VERSION}" == *rc* ]]; then
-    # rc's get "rc" without the number
-    MAJOR_EXT="${MAJOR_EXT}rc"
-    SOVER_EXT="${SOVER_EXT}rc"
+    # rc's get "-rcX" suffix; in this case, PKG_VERSION has 4 parts,
+    # (e.g. 19.1.0.rc1), so take the last part after splitting on "."
+    MAJOR_EXT="${MAJOR_EXT}-${VER_ARR[3]}"
+    SOVER_EXT="${SOVER_EXT}-${VER_ARR[3]}"
 elif [[ "${PKG_VERSION}" == *dev0 ]]; then
     # otherwise with git suffix
     MAJOR_EXT="${MAJOR_EXT}git"
