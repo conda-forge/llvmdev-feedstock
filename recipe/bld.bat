0c711d9071b476792c7e64d8d6edd4004d4e0905
H. Vetinari <h.vetinari@gmx.com>
2024-08-22 17:57:25 +1100

    remove obsolete CMake policy setting
    LLVM now has a minimum of CMake 3.20

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -29,7 +29,6 @@ cmake -G "Ninja" ^
     -DLLVM_UTILS_INSTALL_DIR=libexec\llvm ^
     -DLLVM_BUILD_LLVM_C_DYLIB=ON ^
     -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly ^
-    -DCMAKE_POLICY_DEFAULT_CMP0111=NEW ^
     %SRC_DIR%/llvm
 if %ERRORLEVEL% neq 0 exit 1
 
