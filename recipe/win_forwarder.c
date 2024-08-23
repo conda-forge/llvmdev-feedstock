9a1589c4674f4838e230c966a87daf21d2ac90f0
H. Vetinari <h.vetinari@gmx.com>
2024-08-23 18:50:29 +1100

    only search for versioned binary in folder of wrapper

--- recipe/win_forwarder.c
+++ recipe/win_forwarder.c
@@ -18,11 +18,28 @@ int _tmain( int argc, TCHAR *argv[] )
     // reconstruct commandline call we received, pointing to versioned binary;
     // 32767 == maximum length for lpCommandLine argument
     char forwarded[32767];
-    // the first element is the name of the calling function, which we extend
-    strcat(forwarded, argv[0]);
-    unsigned int len = strlen(argv[0]);
+    // initialize buffer with dummy so that strlen below is not UB
+    strcat(forwarded, "xxx");
+
+    // argv[0] is the name of the calling function, which might be a relative path;
+    // for security reasons, get the absolute path of the binary we're calling from
+    // with help from the MIT-licensed https://github.com/gpakosz/whereami
+    void* addr = _ReturnAddress();
+    HMODULE hm = NULL;
+    // non-zero return means success
+    if (GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS |
+                          GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+                          (LPCSTR) addr, &hm))
+        GetModuleFileNameA(hm, forwarded, sizeof(forwarded));
+
+    unsigned int len = strlen(forwarded);
+    if (len < 4) {
+        // realistically, minimum len(r"C:\a") == 4; dummy value has length 3
+        printf( "Could not determine location of calling binary!\n" );
+        return GetLastError();
+    }
     // chop off ".exe" (if the binary we're in was invoked like that)
-    if (len > 4 && strcmp(&forwarded[len-4], ".exe") == 0)
+    if (strcmp(&forwarded[len-4], ".exe") == 0)
         // set null-terminator to finish the string early
         forwarded[len-4] = '\0';
     // point to versioned binary
