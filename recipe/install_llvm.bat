a8e62c91f7105627ca8f75901ddc651a6fd9f210
scott hain <shain@anaconda.com>
2025-04-18 07:53:38 -0700

    Tweak the cmake vars and add correct sed for dia
    Signed-off-by: scott hain <shain@anaconda.com>

--- recipe/install_llvm.bat
+++ recipe/install_llvm.bat
@@ -1,14 +1,16 @@
 @echo on
 
-REM We do build llvm for windows now but not LLVM-C or LLVM-C{{ version }} There's an LLVM-c.dll, which implements
-REM the C (but not C++) API, which can be built if we have -DLLVM_BUILD_LLVM_C_DYLIB=ON at the build stage. LLVM .dlls
-REM are used for e.g. JIT compilation, which we don't need at the moment on Windows. See the conda-forge recipe for an
-REM example of building and packaging this dll if we do need it.
-
 :: temporary prefix to be able to install files more granularly
 mkdir temp_prefix
 
-if "%PKG_NAME%" == "llvm-tools" (
+
+if "%PKG_NAME%" == "libllvm-c%PKG_VERSION:~0,2%" (
+    cmake --install .\build --prefix=.\temp_prefix
+    if %ERRORLEVEL% neq 0 exit 1
+    REM only libLLVM-C
+    move .\temp_prefix\bin\LLVM-C.dll %LIBRARY_BIN%
+    move .\temp_prefix\lib\LLVM-C.lib %LIBRARY_LIB%
+) else if "%PKG_NAME%" == "llvm-tools" (
     cmake --install ./build --prefix=./temp_prefix
     if %ERRORLEVEL% neq 0 exit 1
 
@@ -20,6 +22,15 @@ if "%PKG_NAME%" == "llvm-tools" (
     REM llvmdev: everything else
     cmake --install .\build --prefix=%LIBRARY_PREFIX%
     if %ERRORLEVEL% neq 0 exit 1
+
+    REM upstream picks up diaguids.lib from the windows image, see
+    REM https://github.com/llvm/llvm-project/blob/llvmorg-14.0.6/llvm/lib/DebugInfo/PDB/CMakeLists.txt#L17
+    REM which ultimately derives from VSINSTALLDIR, see
+    REM https://github.com/llvm/llvm-project/blob/llvmorg-14.0.6/llvm/cmake/config-ix.cmake#L516
+    REM and gets hardcoded by CMake to point to the path in our windows image.
+    REM This makes it non-portable between image versions (e.g. 2019 vs 2022), so replace
+    REM the hardcoded path with a variable again
+    sed -i "s,C:/Program Files/Microsoft Visual Studio/2022/Enterprise,$ENV{VSINSTALLDIR},g" %LIBRARY_LIB%\cmake\llvm\LLVMExports.cmake
 )
 
 rmdir /s /q temp_prefix
