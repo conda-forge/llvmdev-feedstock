79c0a5efe7e8aad0cd5fde3d7d2ad4edca9d35cd
Isuru Fernando <isuruf@gmail.com>
2026-03-04 12:52:02 -0800

    Fix variable expansion syntax in bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -21,8 +21,9 @@ if NOT "%target_platform%"=="%build_platform%" (
     echo set(LLVM_ENABLE_ZSTD "OFF")                      >> cross-toolchain.cmake
     echo set(LLVM_ENABLE_LIBXML2 "OFF")                   >> cross-toolchain.cmake
     echo set(LLVM_ENABLE_ZLIB "OFF")                      >> cross-toolchain.cmake
-    echo set(CMAKE_LIBRARY_PATH "%LIB_FOR_BUILD")         >> cross-toolchain.cmake
-    echo set(CMAKE_INCLUDE_PATH "%INCLUDE_FOR_BUILD")     >> cross-toolchain.cmake
+    echo set(CMAKE_LIBRARY_PATH "%LIB_FOR_BUILD%")        >> cross-toolchain.cmake
+    echo set(CMAKE_INCLUDE_PATH "%INCLUDE_FOR_BUILD%")    >> cross-toolchain.cmake
+    type cross-toolchain.cmake
     set "CMAKE_ARGS=%CMAKE_ARGS% -DCROSS_TOOLCHAIN_FLAGS_NATIVE=-DCMAKE_TOOLCHAIN_FILE=%cd%\\cross-toolchain.cmake"
 )
 
